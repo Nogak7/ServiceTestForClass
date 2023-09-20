488305bf72feac4d2e60f1b5a27176c5e4b60709
User <User@LAB3-3>
2023-09-20 15:05:01 +0300

    g

--- ServiceTest/Services/TriviaService.cs
+++ ServiceTest/Services/TriviaService.cs
@@ -2,11 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace ServiceTest.Services
 {
-    internal class TriviaService
-    {
+    public class TriviaService
+    { 
+        private HttpClient Client { get; set; }
+        private JsonSerializerOptions JSO { get; set; }
+        const string URL = @" https://zr8z94hw-44376.euw.devtunnels.ms/AmericanQuestions/";
+        public TriviaService() 
+        {
+            Client = new HttpClient();
+           JSO = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true , WriteIndented = true};
+        }
+        public async Task<string> CheackConectionStatus()
+        {
+            try
+            {
+                var response = await Client.GetAsync($"{URL}0GetQuestions");
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    return content;
+                }
+                else
+                    return "Error";
+            }
+            catch { }
+            return null;
+        }
     }
 }
